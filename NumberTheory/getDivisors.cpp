d5f0248e2d6fbf1627f76343443830fe5f2056c8
abdelrahmanAE <53497149+abdelrahmanAE@users.noreply.github.com>
2026-06-30 16:12:23 +0300

    Create getDivisors.cpp

---
+++ NumberTheory/getDivisors.cpp
@@ -0,0 +1,14 @@
+vector<int> getDivisors(int n){   //O(sqrt(n))
+    vector<int>ret;
+    int i = 1;
+    for(i = 1; i*i < n; i++){
+        if(n%i == 0){
+            ret.push_back(i);
+            ret.push_back(n/i);
+        }
+    }
+    if(i*i == n){
+        ret.push_back(i);
+    }
+    return ret;
+}
